86fccf08677ac7538214caf591aecd90e4c949f4
Chandrakant15 <chandrakant.gawde@finmeadows.com>
2025-11-20 13:21:06 +0530

    Project structure

---
+++ src/main/java/com/ecommerce/project/SdEcomApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdEcomApplication.class, args);
+	}
+
+}
