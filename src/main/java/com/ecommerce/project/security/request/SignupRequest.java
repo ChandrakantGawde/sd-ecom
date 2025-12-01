059d0e6d81804315edb95c1f350ff54534e6c50b
Chandrakant15 <chandrakant.gawde@finmeadows.com>
2025-12-01 12:29:55 +0530

    signin API

---
+++ src/main/java/com/ecommerce/project/security/request/SignupRequest.java
@@ -0,0 +1,5 @@
+package com.ecommerce.project.security.request;
+
+public class SignupRequest {
+
+}
---
+++ src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ecommerce.project.security.services;
+
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional // Entire operation done or everything's role back
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User Not Found with username: "+ username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
